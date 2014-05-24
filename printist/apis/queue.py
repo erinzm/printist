86b7888aa5dfe0164e6e81873bf703b3b9930d86
Liam M <archimedespi3141@gmail.com>
2014-05-24 11:32:16 -0500

    Implemented queueing scaffolding

--- printist/apis/queue.py
+++ printist/apis/queue.py
@@ -1,3 +1,38 @@
 from flask import Blueprint, abort, jsonify
 
 queue = Blueprint('queue', __name__)
+
+"""
+Get currently queued parts
+"""
+@queue.route('/queue', methods=['GET'])
+def _GET_queue():
+  abort(501)
+
+"""
+Push a filename onto the queue
+"""
+@queue.route('/queue', methods=['POST'])
+def _POST_queue():
+  abort(501)
+
+"""
+Delete a part from the queue by its queueid
+"""
+@queue.route('/queue/<id:int>',  methods=['DELETE'])
+def _DELETE_queue_part(idf):
+  abort(501)
+
+"""
+Start the print queue
+"""
+@queue.route('/queue/print', methods=['POST'])
+def _POST_queue_print():
+  abort(501)
+
+"""
+Stop the print queue
+"""
+@queue.route('/queue/print', methods=['DELETE'])
+def _DELETE_queue_print():
+  abort(501)
