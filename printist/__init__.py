bac642a8a933942d30de8912192e6a93958e2843
ArchimedesPi <archimedespi3141@gmail.com>
2014-05-25 10:54:46 -0500

    Remove config call

--- printist/__init__.py
+++ printist/__init__.py
@@ -4,7 +4,6 @@ from apis.files import files
 import config
 
 printist = Flask(__name__)
-printist.config.from_object(config)
 
 printist.register_blueprint(printer, url_prefix='/api')
 printist.register_blueprint(files, url_prefix='/api')
